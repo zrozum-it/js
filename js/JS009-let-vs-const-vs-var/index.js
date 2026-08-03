ea354a169bdff03f3cbea8d8c29cc760fe804af4
main <designbymilencja@gmail.com>
2026-08-03 14:21:00 +0200

    feat: add JS009

---
+++ js/JS009-let-vs-const-vs-var/index.js
@@ -0,0 +1,12 @@
+// JS009 — let, const, var
+
+// Trzy słowa kluczowe służące do tworzenia zmiennych.
+
+let firstName;
+
+var lastName;
+
+const country = "Poland";
+
+// const musi otrzymać wartość już w momencie deklaracji.
+// const city; ❌
