471850eba55275eb73b32659f2d42996c23c7371
main <designbymilencja@gmail.com>
2026-08-07 13:13:44 +0200

    feat: add JS012 about string type

---
+++ js/JS012-string/index.js
@@ -0,0 +1,9 @@
+// JS012 — String
+
+const firstName = "Milena";
+const city = "Garwolin";
+const message = "Hello, JavaScript!";
+
+console.log(firstName);
+console.log(city);
+console.log(message);
